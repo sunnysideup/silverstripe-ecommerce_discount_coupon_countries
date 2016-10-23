7ea5e5c66345d37a0a04434efbf2f9953c3f8ddc
Nicolaas <github@sunnysideup.co.nz>
2016-10-23 14:24:50 +1300

    BUGFIXES: various

--- code/model/DiscountCouponCountriesModifierExtension.php
+++ code/model/DiscountCouponCountriesModifierExtension.php
@@ -1,12 +1,12 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class DiscountCouponCountriesModifierExtension extends DataExtension 
+class DiscountCouponCountriesModifierExtension extends DataExtension
 {
 
     /**
@@ -14,53 +14,48 @@ class DiscountCouponCountriesModifierExtension extends DataExtension
      * is not in the included list or if it is in the excluded list
      * in all other cases it returns null to basically ignore the country specific
      * rules
-     * 
-     * @return bool
-     * 
+     *
+     * @param DiscountCouponOption $coupon
+     *
+     * @return bool | null
+     *
      */
     function checkForExclusions($coupon)
     {
-        $countryCode = null;
-        if($order = $this->owner->Order()) {
-            if($order->OriginatingCountryCode){
-               $countryCode = $order->OriginatingCountryCode;
-            }
-        }
-        if(! $countryCode) {
-            $countryCode = CountryPrice_EcommerceCountry::get_real_country();
-        }
-        
-        
+        $countryCode = CountryPrice_EcommerceCountry::get_real_country();
         if($countryCode) {
-
+            $countryCode = $countryCode->Code;
             $includedCountries = $coupon->IncludedCountries();
-            $excludedCountries = $coupon->ExcludedCountries();        
-            
+            $excludedCountries = $coupon->ExcludedCountries();
+
             //first situation: no country information => ALLOW => return NULL to ignore this.
             if($includedCountries->count() == 0 && $excludedCountries->count() == 0){
+                $this->owner->DebugString .= '--- no country rules apply ---';
                 return null;
             }
 
             //second situation: includes and excludes
             if($includedCountries->count() > 0 && $excludedCountries->count() > 0){
+                $this->owner->DebugString .= '--- inclusions and exclusions apply - checking for '.$countryCode.' ---';
                 $returnFlag = true;
-                
+
                 $includeArray = $includedCountries->column('Code');
                 if(in_array($countryCode, $includeArray)){
                     $returnFlag = null;
                 }
-                
+
                 $excludeArray = $excludedCountries->column('Code');
                 if(in_array($countryCode, $excludeArray)) {
                     $returnFlag = true;
                 }
-                
+
                 return $returnFlag;
-                      
+
             }
 
             //third situation: is it included?
             if($includedCountries->count() > 0){
+                $this->owner->DebugString .= '--- inclusions apply - checking for '.$countryCode.' ---';
                 $includeArray = $includedCountries->column('Code');
                 if(in_array($countryCode, $includeArray)) {
                     return null;
@@ -72,6 +67,7 @@ class DiscountCouponCountriesModifierExtension extends DataExtension
 
             //fourth situation: is it excluded?
             if($excludedCountries->count() > 0){
+                $this->owner->DebugString .= '--- exclusions apply - checking for '.$countryCode.' ---';
                 $excludeArray = $excludedCountries->column('Code');
                 if(in_array($countryCode, $excludeArray)) {
                     return true;
@@ -80,7 +76,10 @@ class DiscountCouponCountriesModifierExtension extends DataExtension
                     //return true;
                 }
             }
+        } else {
+            $this->owner->DebugString .= '--- no country code could be found ---';
         }
+
         return null;
-    }    
+    }
 }
--- code/model/DiscountCouponOptionCountriesExtension.php
+++ code/model/DiscountCouponOptionCountriesExtension.php
@@ -1,44 +1,44 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-class DiscountCouponOptionCountriesExtension extends DataExtension 
+class DiscountCouponOptionCountriesExtension extends DataExtension
 {
-    
+
     private static $casting = array(
         "CountryDescription" => "Varchar"
     );
-    
+
     private static $summary_fields = array(
         "CountryDescription" => "CountryDescription"
     );
 
-        
+
     private static $field_labels = array(
         "CountryDescription" => "Where"
     );
 
-    
+
     private static $many_many = array(
         "IncludedCountries" => "EcommerceCountry",
         "ExcludedCountries" => "EcommerceCountry"
     );
-    
+
 //    private static $belongs_many_many = array(
-//        
+//
 //    );
 
-    
-    
+
+
     function getCountryDescription()
     {
         $returnString = '';
         $includedCountries = $this->owner->IncludedCountries();
-        $excludedCountries = $this->owner->ExcludedCountries();      
+        $excludedCountries = $this->owner->ExcludedCountries();
         //first situation: no country information => ALLOW => return NULL to ignore this.
         if($includedCountries->count() == 0 && $excludedCountries->count() == 0){
             return _t('DiscountCouponOptionCountriesExtension.AVAILABLE_IN_ALL_COUNTRIES', 'Available in all countries');
@@ -46,7 +46,15 @@ class DiscountCouponOptionCountriesExtension extends DataExtension
 
         //second situation: includes and excludes
         if($includedCountries->count() > 0 && $excludedCountries->count() > 0){
-            return 'marcel to fix';
+            $includeArray = $includedCountries->column('Code');
+            $excludeArray = $excludedCountries->column('Code');
+            foreach($includeArray as $key => $countryCode) {
+                if(in_array($countryCode, $excludeArray)) {
+                    unset($includeArray[$key]);
+                }
+            }
+            return  _t('DiscountCouponOptionCountriesExtension.AVAILABLE_IN', 'Available in: ').implode(', ', $includeArray).
+                    _t('DiscountCouponOptionCountriesExtension.SPECIFICALLY_EXCLUDED', ' || Specifically excluded: ').implode(', ', $excludeArray);
             //...
         }
 
